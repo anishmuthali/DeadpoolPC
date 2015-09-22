fe43bea93000a83139927636b1db12c1054bd79a
anishmuthali <anishmuthali@gmail.com>
2015-09-22 15:51:56 -0700

    created project8while.java

---
+++ project8while.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class project8while{
+  public void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    int input = in.nextInt();
+    while (input > 0){
+      int binaryNum = input % 2;
+    }
+  
+  }
+}  
