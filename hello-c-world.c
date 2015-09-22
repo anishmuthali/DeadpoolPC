4f6ae011a72b6b705daf294be2065bf51998d98b
anishmuthali <anishmuthali@gmail.com>
2015-09-21 23:17:26 -0700

    changed input to number

--- hello-c-world.c
+++ hello-c-world.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main(void){
-    char* name;
-    printf("Enter your name: ");
-    scanf("%[^\n]", name);
-    printf("%s", name);
-    return 0;
+    int a;
+    printf("Please enter an integer: ")
+    scanf("%d", a);
+    int b;
+    b = a * 2;
+    printf("%d", b);
 }
