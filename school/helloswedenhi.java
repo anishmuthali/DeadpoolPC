352f8a538a8da9e5c4831833ec1f84182930a6a0
DEADPOOL21 <anishmuthali@gmail.com>
2015-09-22 21:09:38 -0700

    Added a folder for school

---
+++ school/helloswedenhi.java
@@ -0,0 +1,9 @@
+public class helloswedenhi{
+	public static void main(String[] args){
+		System.out.println("Good Morning. --> God morgon.");
+		System.out.println ("It is a pleasure to meet you. --> Det ar ett noje att traffa dig.");
+		System.out.println("Please call me tomorrow. --> Ring mig i morgon.");
+		System.out.println("Have a nice day! --> Ha en bra dag!");
+	}
+}
+
