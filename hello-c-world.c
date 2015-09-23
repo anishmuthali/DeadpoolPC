9661b2c92dea75bb303bec6d888d7dbcaf7d3cec
DEADPOOL21 <anishmuthali@gmail.com>
2015-09-22 22:15:06 -0700

    Added exe's and updated some bugs in c source files

--- hello-c-world.c
+++ hello-c-world.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void){
     int a;
-    printf("Please enter an integer: ")
-    scanf("%d", a);
+    printf("Please enter an integer: ");
+    scanf("%d", &a);
     int b;
     b = a * 2;
     printf("%d", b);
+    sleep(10);
+    return 0;
 }
--- hello.c
+++ hello.c
@@ -1,6 +1,8 @@
-#include <stdio>
+#include <stdio.h>
+#include <unistd.h>
 
 int main(void) {
   /* code */
   printf("Hello, world");
+  sleep(10);
 }
